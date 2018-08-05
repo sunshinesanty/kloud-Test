df54538cfb596293022b88a33f1c52f1765b550a
sunshinesanty <iyer.santoshkumar@gmail.com>
2018-08-06 04:48:25 +1000

    New Read CSV

---
+++ ReadCSV/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.Json;
+
+namespace HelloWorldLambda
+{
+    public class Handler
+    {
+        [LambdaSerializer(typeof(JsonSerializer))]
+        public Result Handle(Request request)
+        {
+            return new Result
+            {
+                HelloWorld = request.Name
+            };
+        }
+    }
+
+    public class Request
+    {
+        public string Name { get; set; }
+    }
+
+    public class Result
+    {
+        public string HelloWorld { get; set; }
+    }
+}
